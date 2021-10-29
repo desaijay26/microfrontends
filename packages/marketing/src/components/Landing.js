69761b183470c8139b894bf51331401aa545bbc0
desaijay26 <desai26jay@gmail.com>
2021-10-29 14:53:13 +0530

    deployment

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -135,22 +135,6 @@ export default function Album() {
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
     </React.Fragment>
   );
 }
